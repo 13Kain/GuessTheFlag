b7ae46bbd3086518fcbec0b77de57a89fab930b8
Nikita Yashin <n.yashin88@icloud.com>
2022-08-14 08:51:11 +0300

    Initial Commit

---
+++ GuesssTheFlag/GuesssTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuesssTheFlagApp.swift
+//  GuesssTheFlag
+//
+//  Created by Nikita Yashin on 14.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuesssTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
